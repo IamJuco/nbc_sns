f0ed590734cf85f102693f6dc68fc146f04821e3
koreabgj <ligerbae@gmail.com>
2024-04-02 20:35:16 +0900

    feat/login

---
+++ app/src/main/java/com/example/nbc_sns/Comment.kt
@@ -0,0 +1,6 @@
+package com.example.nbc_sns
+
+data class Comment (
+    val userId: Int,
+    val content: String,
+)
---
+++ app/src/main/java/com/example/nbc_sns/UserManager.kt
@@ -0,0 +1,28 @@
+package com.example.nbc_sns
+
+object UserManager {
+
+    private val users: HashMap<String, UserInfo> = hashMapOf() // Key: UserId , value: UserInfo
+
+    fun getUser(userId: String): UserInfo? {
+        return users[userId]
+    }
+
+    fun register(userInfo: UserInfo): Boolean {
+        // 이미 존재하는 아이디와 겹치는지 검증
+        if (userInfo.id in users) {
+            return false // 가입 처리가 안 된 것
+        }
+        users[userInfo.id] = userInfo
+        return true
+    }
+
+    // 존재하지 않는 회원에 대해 회원 탈퇴를 시도할 경우 false, 아니면  true를 반환함
+    fun unregister(userId: String): Boolean {
+        return users.remove(userId) != null
+    }
+
+    fun isUserRegistered(email: String, password: String): Boolean {
+        TODO("Not yet implemented")
+    }
+}

67aa7296e030269fee25a03e6feac387de19865d
DoDoBest <send1684@gmail.com>
2024-04-02 13:04:37 +0900

    feat: 선택된 이미지 라운드에 색상을 지정해서 눈에 띄도록 UI 설정

--- app/src/main/java/com/example/nbc_sns/ui/createPost/GalleryAdapter.kt
+++ app/src/main/java/com/example/nbc_sns/ui/createPost/GalleryAdapter.kt
@@ -3,9 +3,11 @@ package com.example.nbc_sns.ui.createPost
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.nbc_sns.R
 import com.example.nbc_sns.databinding.ItemGalleryBinding
 
 class GalleryAdapter(
@@ -18,16 +20,20 @@ class GalleryAdapter(
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(galleryItem: GalleryItem) {
+            // TODO : MainThread에서 처리하다보니 프레임 드랍 발생. Glide를 사용하거나,
+            //  Coroutine을 이용해 IO 쓰레드에서 처리하도록 바꿔야 함
             binding.imageViewThumbnail.setImageURI(galleryItem.uri)
 
             if (galleryItem.id !in selectedItems) {
                 binding.tvSelected.visibility = View.INVISIBLE
+                binding.root.background = null
             } else {
                 val index = selectedItems.keys.indexOf(galleryItem.id) + 1
                 if (index != 0) {
                     binding.tvSelected.text = index.toString()
                 }
                 binding.tvSelected.visibility = View.VISIBLE
+                binding.root.background = AppCompatResources.getDrawable(binding.root.context, R.drawable.selected_item_background)
             }
 
             binding.root.setOnClickListener {

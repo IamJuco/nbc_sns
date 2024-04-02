41bb04b1429fe8ce0adfce0c3c1c92409d4e43c4
koreabgj <ligerbae@gmail.com>
2024-04-02 20:25:47 +0900

    로그인, 회원가입 액티비티 초안

--- app/src/main/java/com/example/nbc_sns/LogInActiviy.kt
+++ app/src/main/java/com/example/nbc_sns/LogInActiviy.kt
@@ -1,12 +1,14 @@
 package com.example.nbc_sns
 
 import android.content.Intent
-import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nbc_sns.databinding.ActivityLogInBinding
-import java.util.Locale
+
+private fun <E> List<E>.isUserRegistered(email: E, password: E): Boolean {
+    TODO("Not yet implemented")
+}
 
 class LogInActivity : AppCompatActivity() {
 
@@ -28,6 +30,7 @@ class LogInActivity : AppCompatActivity() {
             val putPassword = if (isEnglish) R.string.put_password else R.string.put_password1
             val login = if (isEnglish) R.string.login else R.string.login1
             val register = if (isEnglish) R.string.register else R.string.register1
+
             val confirmEmail = if (isEnglish) R.string.confirm_email else R.string.confirm_email1
             val loginSucceed = if (isEnglish) R.string.login_succeed else R.string.login_succeed1
             val unregisteredEmail = if (isEnglish) R.string.unregistered_email else R.string.unregistered_email1
@@ -37,6 +40,7 @@ class LogInActivity : AppCompatActivity() {
             binding.etPw.hint = getString(putPassword)
             binding.buttonLogin.text = getString(login)
             binding.buttonRegister.text = getString(register)
+
             var text1 = "아이디와 비밀번호를 확인해 주세요"
             text1 = getString(confirmEmail)
             var text2 = "로그인 성공"
@@ -49,40 +53,43 @@ class LogInActivity : AppCompatActivity() {
         binding.ivTeam
 
         binding.buttonLogin.setOnClickListener {
-            val ID = binding.etEmail.text.toString()
-            val PW = binding.etPw.text.toString()
+            val id = binding.etEmail.text.toString()
+            val pw = binding.etPw.text.toString()
 
-            if (ID.isBlank() || PW.isBlank()) {
+            if (id.isBlank() || pw.isBlank()) {
                 Toast.makeText(this, "아이디와 비밀번호를 확인해 주세요", Toast.LENGTH_SHORT).show()
 
             } else {
                 val email = binding.etEmail.text.toString()
                 val password = binding.etPw.text.toString()
-                val registeredEmails = listOf("user1@example.com", "user2@example.com", "user3@example.com")
 
-                if (registeredEmails.contains(email)) {
+                // 사용자 회원가입 여부 확인
+                if (email != null && password != null && UserManager.isUserRegistered(email, password)) {
                     Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
                     val intent = Intent(this, RegisterActivity::class.java)
                     // 임시로 Main 대신 RegisterActivity로 이동
                     startActivity(intent)
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
 
                 } else {
-                    Toast.makeText(this, "등록되지 않은 이메일입니다", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "등록되지 않은 사용자입니다", Toast.LENGTH_SHORT).show()
                 }
+            }
+        }
 
-                // 로그인 이후 사용자 이름이 화면에서 보이게 하기
-//                val userData = UserManager.getUserData()
-//                if (userData != null) {
-//                    val name = userData.name
-//                    val email = userData.email
+                  // 로그인 이후 사용자 이름이 보이게 하기
+//                fun getUserInfo(): UserInfo {
+//                    return UserInfo(
+//                        email = "example@email.com",
+//                        name = "User",
+//                        )
 //                }
-            }
 
-            binding.buttonRegister.setOnClickListener {
-                val intent = Intent(this, RegisterActivity::class.java)
-                startActivity(intent)
-                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-            }
+
+        binding.buttonRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         }
     }
 }
--- app/src/main/java/com/example/nbc_sns/RegisterActivity.kt
+++ app/src/main/java/com/example/nbc_sns/RegisterActivity.kt
@@ -26,19 +26,21 @@ class RegisterActivity : AppCompatActivity() {
             var isEnglish = false
             isEnglish = !isEnglish
 
-            val name = if (isEnglish) R.string.name else R.string.name1
-            val email = if (isEnglish) R.string.email else R.string.email1
-            val password = if (isEnglish) R.string.password else R.string.password1
-            val rePassword = if (isEnglish)  R.string.re_password else R.string.re_password1
             val putName = if (isEnglish) R.string.put_name else R.string.put_name1
             val putEmail = if (isEnglish) R.string.put_email else R.string.put_email1
             val putPassword = if (isEnglish) R.string.put_password else R.string.put_password1
             val putRePassword = if (isEnglish)  R.string.put_re_password else R.string.put_re_password1
             val register = if (isEnglish) R.string.register else R.string.register1
+
+            val name = if (isEnglish) R.string.name else R.string.name1
+            val email = if (isEnglish) R.string.email else R.string.email1
+            val password = if (isEnglish) R.string.password else R.string.password1
+            val rePassword = if (isEnglish)  R.string.re_password else R.string.re_password1
+
             val validEmail = if (isEnglish) R.string.valid_email else R.string.valid_email1
             val passwordLength = if (isEnglish) R.string.password_length else R.string.password_length1
-            val information = if (isEnglish) R.string.information else R.string.information1
             val passwordMatch = if (isEnglish) R.string.password_match else R.string.password_match1
+            val information = if (isEnglish) R.string.information else R.string.information1
 
             binding.etName.hint = getString(putName)
             binding.etEmail.hint = getString(putEmail)
@@ -54,12 +56,13 @@ class RegisterActivity : AppCompatActivity() {
             text3 = getString(password)
             var text4 = "비밀번호 확인"
             text4 = getString(rePassword)
+
             var text5 = "유효한 이메일을 입력해 주세요"
-            text5 = getString(putEmail)
+            text5 = getString(validEmail)
             var text6 = "비밀번호는 8자 이상이어야 합니다"
-            text6 = getString(putPassword)
+            text6 = getString(passwordLength)
             var text7 = "비밀번호가 일치하지 않습니다"
-            text7 = getString(putRePassword)
+            text7 = getString(passwordMatch)
             var text8 = "입력되지 않은 정보가 있습니다"
             text8 = getString(information)
         }
@@ -75,13 +78,13 @@ class RegisterActivity : AppCompatActivity() {
 
         binding.btnRegister.setOnClickListener {
             val name = binding.etName.text.toString()
-            val ID = binding.etEmail.text.toString()
-            val PW = binding.etPw.text.toString()
-            val rePW = binding.etRePw.text.toString()
+            val id = binding.etEmail.text.toString()
+            val pw = binding.etPw.text.toString()
+            val rePw = binding.etRePw.text.toString()
 
             val emailPattern = Regex("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
-            val isEmailValid = emailPattern.matches(ID)
-            val isPasswordValid = PW.length >= 8
+            val isEmailValid = emailPattern.matches(id)
+            val isPasswordValid = pw.length >= 8
 
             if (!isEmailValid) {
                 Toast.makeText(this, "유효한 이메일을 입력해 주세요", Toast.LENGTH_SHORT).show()
@@ -89,10 +92,12 @@ class RegisterActivity : AppCompatActivity() {
             } else if (!isPasswordValid) {
                 Toast.makeText(this, "비밀번호는 8자 이상이어야 합니다", Toast.LENGTH_SHORT).show()
 
-            } else if (PW != rePW) {
+            } else if (pw != rePw) {
                 Toast.makeText(this, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
 
-            } else if (name.isBlank() || ID.isBlank() || PW.isBlank() || rePW.isBlank()) {
+            } else if (
+                name.isNullOrBlank() || id.isNullOrBlank() || pw.isNullOrBlank() || rePw.isNullOrBlank()
+                ) {
                 Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
 
             } else {
